6b9417435b5ae16dfcf630d08fd8d1ef0ad6cf16
Kyrylo <Kyrylo@DESKTOP-CBEBKS8>
2022-03-16 01:51:08 +0200

    Add project files.

---
+++ WordCounterMVC/Controllers/WordController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WordCounterMVC.Models;
+
+namespace WordCounterMVC.Controllers
+{
+    public class WordController : Controller
+    {
+        public IActionResult Index()
+        {
+            TextParse text = new TextParse("Hello, my dear friends");
+            WordAnalysis wordAnalysis = new WordAnalysis(text);
+
+            return View(wordAnalysis.GetTopWordPharse());
+        }
+    }
+}
